500f5b90edb8ee975783cff61e068d905f4b759d
Leo Gaggl <leo@brightcookie.com.au>
2013-08-26 12:39:51 +0930

    Initial commit of scanbuttond scanner script for Fujitsu S1500 ADF scanner

---
+++ buttonpressed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+OUT_DIR=/home/USERID/scan
+TMP_DIR=`mktemp -d`
+FILE_NAME=scan_`date +%Y%m%d-%H%M%S`
+cd $TMP_DIR
+echo "################## Scanning ###################"
+scanimage --resolution 150 --batch=scan_%03d.pnm --format=pnm --mode Gray --device-name "fujitsu:ScanSnap S1500:67953" --page-width 210 --page-height 297 --sleeptimer 1 -y 297 -x 210
+echo "############## Converting to PDF ##############"
+mogrify -format tif *.pnm
+
+echo "################ Cleaning Up ################"
+
+for f in ./*.tif; do
+   tesseract "$f" "$f" -l eng hocr
+   hocr2pdf -i "$f" -s -o "$f.pdf" < "$f.html"
+done
+pdftk *.tif.pdf cat output $FILE_NAME.pdf && rm *.tif.pdf && rm *.tif.html
+
+wput $FILE_NAME.pdf ftp://uid:pwd@scanner.domain:21/Alfresco/scans/
+
+cp $FILE_NAME.pdf $OUT_DIR/
+cd ..
+rm -rf $TMP_DIR
+
